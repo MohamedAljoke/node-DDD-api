dd221e6e95119287e4729623e2fe64062fdfd7ae
Mohamed <mohammed.mmaljoke@gmail.com>
2022-11-19 20:41:08 -0300

    test: ensure signup controller returns 400 if password does not match

--- src/presentation/controllers/signup.spec.ts
+++ src/presentation/controllers/signup.spec.ts
@@ -81,6 +81,22 @@ describe('SignUp Controller', () => {
       new MissingParamsError('passwordConfirmation')
     );
   });
+  test('Should return 400 if no password confirmation fails', () => {
+    const { sut } = makeSut();
+    const httpRequest = {
+      body: {
+        name: 'any_name',
+        email: 'any_email@mail.com',
+        password: 'any_password',
+        passwordConfirmation: 'invalid_password',
+      },
+    };
+    const httpResponse = sut.handle(httpRequest);
+    expect(httpResponse.statusCode).toBe(400);
+    expect(httpResponse.body).toEqual(
+      new InvalidParamError('passwordConfirmation')
+    );
+  });
   test('Should return 400 if invalid email is provided', () => {
     const { sut, emailValidatorStub } = makeSut();
     jest.spyOn(emailValidatorStub, 'isValid').mockReturnValueOnce(false);
