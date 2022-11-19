f427ef624636402d2993804de942764affa8152e
Mohamed <mohammed.mmaljoke@gmail.com>
2022-11-19 11:53:37 -0300

    fix: jest config file

--- jest.config.js
+++ jest.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-    roots: ['<rootDir>/tests'],
+    roots: ['<rootDir>/src'],
     collectCoverageFrom: [
       '<rootDir>/src/**/*.ts',
       '!<rootDir>/src/main/**'
@@ -7,12 +7,12 @@ module.exports = {
     coverageDirectory: 'coverage',
     coverageProvider: 'babel',
     testEnvironment: 'node',
-    preset: '@shelf/jest-mongodb',
+    // preset: '@shelf/jest-mongodb',
     transform: {
       '.+\\.ts$': 'ts-jest'
-    },
-    moduleNameMapper: {
-      '@/tests/(.*)': '<rootDir>/tests/$1',
-      '@/(.*)': '<rootDir>/src/$1'
     }
+    // moduleNameMapper: {
+    //   '@/tests/(.*)': '<rootDir>/tests/$1',
+    //   '@/(.*)': '<rootDir>/src/$1'
+    // }
   }
