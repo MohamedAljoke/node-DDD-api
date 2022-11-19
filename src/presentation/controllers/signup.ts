04ba5a45def92bc41e07c96f44091503167fb532
Mohamed <mohammed.mmaljoke@gmail.com>
2022-11-19 20:01:09 -0300

    feat: return 500 if email validator throws

--- src/presentation/controllers/signup.ts
+++ src/presentation/controllers/signup.ts
@@ -1,5 +1,6 @@
 import { InvalidParamError } from '../errors/invalid-param-error';
 import { MissingParamsError } from '../errors/missing-params-error';
+import { ServerError } from '../errors/server-error';
 import { badRequest } from '../helpers/http-helper';
 import { Controller } from '../protoccols/controller';
 import { EmialValidator } from '../protoccols/email-validator';
@@ -22,13 +23,20 @@ export class SignUpController implements Controller {
         return badRequest(new MissingParamsError(field));
       }
     }
-    const isValidEmail = this.emailValidator.isValid(httpRequest.body.email);
-    if (!isValidEmail) {
-      return badRequest(new InvalidParamError('email'));
+    try {
+      const isValidEmail = this.emailValidator.isValid(httpRequest.body.email);
+      if (!isValidEmail) {
+        return badRequest(new InvalidParamError('email'));
+      }
+      return {
+        body: 'a',
+        statusCode: 400,
+      };
+    } catch (e) {
+      return {
+        statusCode: 500,
+        body: new ServerError(),
+      };
     }
-    return {
-      body: 'a',
-      statusCode: 400,
-    };
   }
 }
