32be2df0a17bc8fea2ee10a3aec6e899793f6540
Mohamed <mohammed.mmaljoke@gmail.com>
2022-11-19 19:36:33 -0300

    feat: validate email on signup

--- src/presentation/controllers/signup.ts
+++ src/presentation/controllers/signup.ts
@@ -1,9 +1,15 @@
+import { InvalidParamError } from '../errors/invalid-param-error';
 import { MissingParamsError } from '../errors/missing-params-error';
 import { badRequest } from '../helpers/http-helper';
 import { Controller } from '../protoccols/controller';
+import { EmialValidator } from '../protoccols/email-validator';
 import { HttpResponse, HttpRequest } from '../protoccols/http';
 
 export class SignUpController implements Controller {
+  private readonly emailValidator: EmialValidator;
+  constructor(emailValidator: EmialValidator) {
+    this.emailValidator = emailValidator;
+  }
   handle(httpRequest: HttpRequest): HttpResponse {
     const requiredFields = [
       'email',
@@ -15,6 +21,10 @@ export class SignUpController implements Controller {
       if (!httpRequest.body[field]) {
         return badRequest(new MissingParamsError(field));
       }
+    }
+    const isValidEmail = this.emailValidator.isValid(httpRequest.body.email);
+    if (!isValidEmail) {
+      return badRequest(new InvalidParamError('email'));
     }
     return {
       body: 'a',
