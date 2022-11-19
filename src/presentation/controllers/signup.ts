8a72e17d273cb1825e52efaa757a36eea317d869
Mohamed <mohammed.mmaljoke@gmail.com>
2022-11-19 20:41:01 -0300

    feat: ensure signup controller returns 400 if password does not match

--- src/presentation/controllers/signup.ts
+++ src/presentation/controllers/signup.ts
@@ -13,18 +13,21 @@ export class SignUpController implements Controller {
     this.emailValidator = emailValidator;
   }
   handle(httpRequest: HttpRequest): HttpResponse {
-    const requiredFields = [
-      'email',
-      'name',
-      'password',
-      'passwordConfirmation',
-    ];
-    for (let field of requiredFields) {
-      if (!httpRequest.body[field]) {
-        return badRequest(new MissingParamsError(field));
-      }
-    }
     try {
+      const requiredFields = [
+        'email',
+        'name',
+        'password',
+        'passwordConfirmation',
+      ];
+      for (let field of requiredFields) {
+        if (!httpRequest.body[field]) {
+          return badRequest(new MissingParamsError(field));
+        }
+      }
+      if (httpRequest.body.password !== httpRequest.body.passwordConfirmation) {
+        return badRequest(new InvalidParamError('passwordConfirmation'));
+      }
       const isValidEmail = this.emailValidator.isValid(httpRequest.body.email);
       if (!isValidEmail) {
         return badRequest(new InvalidParamError('email'));
