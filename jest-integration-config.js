3617aa0a09b3c812881f27a5dcbe8a85259d9f76
Mohamed <mohammed.mmaljoke@gmail.com>
2022-11-19 23:58:39 -0300

    chore: add jest scripts

---
+++ jest-integration-config.js
@@ -0,0 +1,4 @@
+const config = require('./jest.config');
+config.testMatch = ['**/*.test.ts'];
+
+module.export = config;
--- jest.config.js
+++ jest.config.js
@@ -1,18 +1,20 @@
 module.exports = {
-    roots: ['<rootDir>/src'],
-    collectCoverageFrom: [
-      '<rootDir>/src/**/*.ts',
-      '!<rootDir>/src/main/**'
-    ],
-    coverageDirectory: 'coverage',
-    coverageProvider: 'babel',
-    testEnvironment: 'node',
-    // preset: '@shelf/jest-mongodb',
-    transform: {
-      '.+\\.ts$': 'ts-jest'
-    },
-    moduleNameMapper: {
-      '@/tests/(.*)': '<rootDir>/tests/$1',
-      '@/(.*)': '<rootDir>/src/$1'
-    }
-  }
+  roots: ['<rootDir>/src'],
+  collectCoverageFrom: [
+    '<rootDir>/src/**/*.ts',
+    '!<rootDir>/src/main/**',
+    '!<rootDir>/node_modules/',
+    '!src/**/index.{js,ts}',
+  ],
+  coverageDirectory: 'coverage',
+  coverageProvider: 'babel',
+  testEnvironment: 'node',
+  // preset: '@shelf/jest-mongodb',
+  transform: {
+    '.+\\.ts$': 'ts-jest',
+  },
+  moduleNameMapper: {
+    '@/tests/(.*)': '<rootDir>/tests/$1',
+    '@/(.*)': '<rootDir>/src/$1',
+  },
+};
