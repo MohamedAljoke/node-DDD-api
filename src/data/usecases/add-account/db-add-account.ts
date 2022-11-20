bb7781e72c18446f71ce0ffb51452ce1ad988a4d
Mohamed <mohammed.mmaljoke@gmail.com>
2022-11-20 02:55:40 -0300

    refactor: move all protocols imports to single file

--- src/data/usecases/add-account/db-add-account.ts
+++ src/data/usecases/add-account/db-add-account.ts
@@ -1,10 +1,9 @@
-import { resolve } from 'path';
-import { AccountModel } from '../../../domain/models/account';
 import {
+  Encrypter,
   AddAccount,
   AddAccountModel,
-} from '../../../domain/usecases/add-account';
-import { Encrypter } from '../../protocols/encrypter';
+  AccountModel,
+} from './db-add-account-protocols';
 
 export class DbAddAccount implements AddAccount {
   private readonly encrypter: Encrypter;
