81335a8068a299ab212683de462d3f0ff521addb
Mohamed <mohammed.mmaljoke@gmail.com>
2022-11-21 17:50:33 -0300

    refactor: helper to change _id to id

--- src/infra/db/mongodb/account-repository/account.ts
+++ src/infra/db/mongodb/account-repository/account.ts
@@ -1,15 +1,14 @@
 import { AddAccountRepository } from '../../../../data/protocols/add-account-repository';
 import { AccountModel } from '../../../../domain/models/account';
 import { AddAccountModel } from '../../../../domain/usecases/add-account';
+import { changeMongoDashedIdToId } from '../helpers/account-mapper';
 import { MongoHelper } from '../helpers/mongo-helper';
 
 export class AccountMongoRepository implements AddAccountRepository {
   async add(addAccount: AddAccountModel): Promise<AccountModel> {
     const accountCollection = MongoHelper.getCollection('accounts');
     const result = await accountCollection.insertOne(addAccount);
-    const resultId = result.insertedId.toString();
-    const account = { ...addAccount, id: resultId };
-    delete account['_id'];
+    const account = changeMongoDashedIdToId<AccountModel>(addAccount);
     return new Promise((resolve) => resolve(account));
   }
 }
