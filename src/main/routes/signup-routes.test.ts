dfb8f472cf32a7627a5d00ef6f37f156533f62d4
Mohamed <mohammed.mmaljoke@gmail.com>
2022-11-23 11:36:35 -0300

    refactor: connect to db befor test

--- src/main/routes/signup-routes.test.ts
+++ src/main/routes/signup-routes.test.ts
@@ -1,7 +1,19 @@
 import request from 'supertest';
+import { MongoHelper } from '../../infra/db/mongodb/helpers/mongo-helper';
 import app from '../config/app';
 
 describe('SignUp Routes', () => {
+  beforeAll(async () => {
+    console.log(process.env.MONGO_URL);
+    const mongoClient = await MongoHelper.connect(process.env.MONGO_URL!);
+  });
+  beforeEach(async () => {
+    const accountCollection = MongoHelper.getCollection('accounts');
+    await accountCollection.deleteMany({});
+  });
+  afterAll(async () => {
+    await MongoHelper.desconnect();
+  });
   test('Should return account on sucess', async () => {
     const data = {
       name: 'random_name',
